b517c60693fa6f1450c6153be67e70c5d825e2f0
shapirodaniel <shapirodanieladam@gmail.com>
2021-04-26 21:58:05 -0500

    add: contact bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Stars from './Stars';
+import Contact from './Contact';
 
 function App() {
 	return (
 		<div className='appContainer'>
 			<Stars />
+			<Contact />
 		</div>
 	);
 }
---
+++ src/Contact.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Contact.css';
+
+const links = [
+	{
+		id: 1,
+		src: '/octocat.png',
+		href: 'https://github.com/shapirodaniel',
+		alt: 'github',
+	},
+	{
+		id: 2,
+		src: '/linkedin.png',
+		href: 'https://linkedin.com/in/shapirodanieladam',
+		alt: 'linkedin',
+	},
+	{
+		id: 3,
+		src: '/medium.png',
+		href: 'https://shapirodanieladam.medium.com',
+		alt: 'medium',
+	},
+];
+
+const Contact = () => {
+	return (
+		<section className='contactContainer'>
+			<div className='copyright'>&#169; 2021 Daniel Shapiro</div>
+			<div className='icons'>
+				{links.map(({ id, src, href, alt }) => (
+					<a key={id} href={href} target='_blank' rel='noreferrer'>
+						<img className='linkIcon' src={src} alt={alt} />
+					</a>
+				))}
+			</div>
+		</section>
+	);
+};
+
+export default Contact;
