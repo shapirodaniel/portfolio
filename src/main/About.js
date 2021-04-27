a41ad83e51bb9e6cc38e665e6e98d6a611e63a6e
shapirodaniel <shapirodanieladam@gmail.com>
2021-04-27 14:36:42 -0500

    add: about cards

--- src/main/About.js
+++ src/main/About.js
@@ -1,10 +1,57 @@
 import React from 'react';
 import './About.css';
 
+const aboutCards = [
+	{
+		id: 1,
+		icon: 'analytics',
+		title: 'Analyst',
+		description: "Sifting piles of data in search of a root cause? I'm there.",
+	},
+	{
+		id: 2,
+		icon: 'next_plan',
+		title: 'Strategist',
+		description: "I'm a proven project manager across multiple industries.",
+	},
+	{
+		id: 3,
+		icon: 'check_circle_outline',
+		title: 'Deliverer',
+		description: 'Teammates trust me to ship features on-time, on-spec.',
+	},
+	{
+		id: 4,
+		icon: 'track_changes',
+		title: 'Chameleon',
+		description: `Design is always in flux—let\'s discover and pivot together.`,
+	},
+];
+
+const AboutCard = ({ card }) => {
+	const { icon, title, description } = card;
+
+	return (
+		<div className='aboutCardContainer'>
+			<span className='iconContainer'>
+				<i className='material-icons'>{icon}</i>
+			</span>
+			<span className='title'>{title}</span>
+			<p>{description}</p>
+		</div>
+	);
+};
+
 const About = () => {
 	return (
 		<section className='aboutContainer'>
-			<h3>About</h3>
+			<h3>ABOUT</h3>
+
+			<div className='aboutCardsList'>
+				{aboutCards.map(card => (
+					<AboutCard key={card.id} card={card} />
+				))}
+			</div>
 		</section>
 	);
 };
