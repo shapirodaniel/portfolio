efc00e29db63c1e2d2063236f7075529f1cbfd18
shapirodaniel <shapirodanieladam@gmail.com>
2021-04-27 08:19:03 -0500

    add: welcome, templates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Welcome from './Welcome';
 import Stars from './Stars';
 import Contact from './Contact';
 
 function App() {
 	return (
 		<div className='appContainer'>
+			<Welcome />
 			<Stars />
 			<Contact />
 		</div>
---
+++ src/Welcome.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Welcome.css';
+
+const Welcome = () => {
+	return (
+		<section className='welcomeContainer'>
+			<h1>
+				Hello, I'm <span className='myName'>Daniel Shapiro.</span>
+				<br />
+				I'm a full-stack software engineer.
+			</h1>
+			<div className='viewMyWork'>
+				<span>View my work</span>
+				<i className='material-icons'>keyboard_arrow_right</i>
+			</div>
+		</section>
+	);
+};
+
+export default Welcome;
