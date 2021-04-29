ba9f3f7e9b31a56870d8b58faf4a21e6fafa2f37
shapirodaniel <shapirodanieladam@gmail.com>
2021-04-29 07:47:57 -0500

    refactor: portfolio

--- src/main/Portfolio.js
+++ src/main/Portfolio.js
@@ -1,69 +1,84 @@
-import React, { useState } from 'react';
+import React from 'react';
+import YoutubeEmbed from './YoutubeEmbed';
 import './Portfolio.css';
 
+// embedId is YouTube embedId
 const portfolioCards = [
 	{
 		id: 1,
-		srcArray: ['/note-ary-1.png', '/note-ary-2.png', '/note-ary-3.png'],
-		name: 'Note-ary',
+		embedId: '2UeevSTOnIs',
+		src: '/note-ary-logo.png',
 		description:
 			'A project management suite with Kanban-style boards and realtime communication support (socket.io)',
-		href: 'https://github.com/shapirodaniel/note-ary',
+		techStack: [
+			{ 'Back-end': ['PostgresSQL + Sequelize', 'Express', 'socket.io'] },
+			{
+				'Front-end': [
+					'React + React Context API',
+					'React Beautiful DnD',
+					'socket.io-client',
+					'vanilla CSS',
+				],
+			},
+		],
+		siteHref: 'https://note-ary.herokuapp.com',
+		githubHref: 'https://github.com/shapirodaniel/note-ary',
 	},
 	{
 		id: 2,
-		srcArray: 'https://i.imgur.com/DBQZsXM.png',
+		embedId: 'https://i.imgur.com/DBQZsXM.png',
 		name: 'The Guessing Game',
 		description:
 			"A detailed look into refactoring an imperative code base to React, React Hooks, and state management via React's Context API",
-		href:
+		techStack: [{ 'Back-end': [] }, { 'Front-end': [] }],
+		siteHref:
 			'https://medium.com/geekculture/rebuilding-an-imperatively-coded-game-from-scratch-in-react-9a082ad002c0',
+		githubHref: 'https://github.com/shapirodaniel/guessing-game-react',
 	},
 	{
 		id: 3,
-		srcArray: 'https://i.imgur.com/UFlcEUI.png',
+		embedId: 'https://i.imgur.com/UFlcEUI.png',
 		name: "Bread Baker's Friend",
 		description:
 			'A web app designed for artisan bakery production floors, allowing dynamic recipe recalculation and alterations, export to dynamic spreadsheets, and recipe file storage + upload',
-		href: 'projectHref',
+		techStack: [{ 'Back-end': [] }, { 'Front-end': [] }],
+		siteHref: 'https://breadbakersfriend.com',
+		githubHref: 'https://github.com/shapirodaniel/breadbakersfriend',
 	},
 ];
 
 const PortfolioCard = ({ card }) => {
-	const { srcArray, name, description, href } = card;
-
-	const [srcIdx, setSrcIdx] = useState(0);
-
-	const handleClick = direction => {
-		if (direction === 'left') {
-			if (srcIdx === 0) {
-				return;
-			}
-			const newIdx = srcIdx - 1;
-			return setSrcIdx(newIdx);
-		}
-		if (srcIdx === srcArray.length - 1) {
-			return;
-		}
-		const newSrcIdx = srcIdx + 1;
-		setSrcIdx(newSrcIdx);
-	};
+	const { embedId, src, description, techStack, siteHref, githubHref } = card;
 
 	return (
-		<div>
-			<div className='portfolioCardContainer'>
-				<img src={srcArray[srcIdx]} alt='note-ary' />
+		<div className='portfolioCardContainer'>
+			<YoutubeEmbed embedId={embedId} />
+			<div className='pc-rightPanel'>
+				<img src={src} alt={'note-ary-logo'} />
+				<p align='center'>{description}</p>
+				{techStack.map(obj => {
+					const [type, list] = Object.entries(obj)[0];
+
+					return (
+						<div>
+							<span>{type}</span>
+							<ul>
+								{list.map(listElem => (
+									<li>{listElem}</li>
+								))}
+							</ul>
+						</div>
+					);
+				})}
 				<div className='btnsContainer'>
-					<span onClick={() => handleClick('left')}>
-						<i className='material-icons'>keyboard_arrow_left</i>
-					</span>
-					<span onClick={() => handleClick('right')}>
-						<i className='material-icons'>keyboard_arrow_right</i>
-					</span>
+					<a href={siteHref} target='_blank' rel='noreferrer'>
+						<span>Visit Site</span>
+					</a>
+					<a href={githubHref} target='_blank' rel='noreferrer'>
+						<span>GitHub Repo</span>
+					</a>
 				</div>
 			</div>
-
-			<a href={href} target='_blank' rel='noreferrer'></a>
 		</div>
 	);
 };
---
+++ src/main/YoutubeEmbed.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './YoutubeEmbed.css';
+
+const YoutubeEmbed = ({ embedId }) => (
+	<div className='video-responsive'>
+		<iframe
+			src={`https://www.youtube.com/embed/${embedId}`}
+			frameBorder='0'
+			allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+			allowFullScreen
+			title='Embedded youtube'
+		/>
+	</div>
+);
+
+export default YoutubeEmbed;
