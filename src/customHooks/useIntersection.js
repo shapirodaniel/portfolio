916f5b1e5b928ff7e50fee37d7eb859a1dc26adb
shapirodaniel <shapirodanieladam@gmail.com>
2021-04-30 13:32:00 -0500

    chore: save useIntersection

--- src/customHooks/useIntersection.js
+++ src/customHooks/useIntersection.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from 'react';
 
-export const useIntersection = () => {
-	return 'hello';
+export const useIntersection = (nodeRef, callback) => {
+
+  let options = {
+    root: document.querySelector(nodeRef.current.id),
+    rootMargin: '0px',
+    threshold: 1.0
+  }
+
+  let observer = new IntersectionObserver(callback, options)
 };
