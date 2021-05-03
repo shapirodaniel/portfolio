7b931731482c6c6d429981e6b9fac262ce847575
shapirodaniel <shapirodanieladam@gmail.com>
2021-05-03 07:11:17 -0500

    chore: move onClick fn to separate statement

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,6 +5,13 @@ import { IntersectionContext } from './context/intersectionContext';
 const Welcome = () => {
 	const { setActiveNodeId } = useContext(IntersectionContext);
 
+	const scrollToAbout = () => {
+		setActiveNodeId('about');
+		document.querySelector('.aboutContainer').scrollIntoView({
+			behavior: 'smooth',
+		});
+	};
+
 	return (
 		<section className='welcomeContainer'>
 			<h1>
@@ -12,15 +19,7 @@ const Welcome = () => {
 				<br />
 				I'm a full-stack software engineer.
 			</h1>
-			<div
-				className='viewMyWork'
-				onClick={() => {
-					setActiveNodeId('about');
-					document.querySelector('.aboutContainer').scrollIntoView({
-						behavior: 'smooth',
-					});
-				}}
-			>
+			<div className='viewMyWork' onClick={scrollToAbout}>
 				<span>View my work</span>
 				<i className='material-icons'>keyboard_arrow_right</i>
 			</div>
