f8efdb202ccd20b44bdd664e00c504eee025df4c
shapirodaniel <shapirodanieladam@gmail.com>
2021-05-03 14:52:23 -0500

    fix: add threshold breakpoints to catch mid-scroll direction changes, useOnScreen

--- src/customHooks/useOnScreen.js
+++ src/customHooks/useOnScreen.js
@@ -3,8 +3,11 @@ import { useState, useEffect } from 'react';
 export const useOnScreen = ref => {
 	const [isOnScreen, setOnScreen] = useState(false);
 
-	const observer = new IntersectionObserver(([entry]) =>
-		setOnScreen(entry.isIntersecting)
+	const observer = new IntersectionObserver(
+		([entry]) => setOnScreen(entry.isIntersecting),
+		{
+			threshold: [0.25, 0.5, 0.75],
+		}
 	);
 
 	useEffect(() => {
