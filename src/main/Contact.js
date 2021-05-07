afaac94fedaf3799083dc7176ba8a5166ed72ce8
shapirodaniel <shapirodanieladam@gmail.com>
2021-05-06 20:19:17 -0500

    add: contact btns to welcome component

--- src/main/Contact.js
+++ src/main/Contact.js
@@ -1,29 +1,9 @@
 import React from 'react';
 import ContactBar from './ContactBar';
+import ContactBtns from './ContactBtns';
 import './Contact.css';
 
 import { useNav } from '../customHooks/useNav';
-
-const links = [
-	{
-		id: 1,
-		src: '/octocat.svg',
-		href: 'https://github.com/shapirodaniel',
-		alt: 'github',
-	},
-	{
-		id: 2,
-		src: '/linked-in.svg',
-		href: 'https://linkedin.com/in/shapirodanieladam',
-		alt: 'linkedin',
-	},
-	{
-		id: 3,
-		src: '/medium.svg',
-		href: 'https://shapirodanieladam.medium.com',
-		alt: 'medium',
-	},
-];
 
 const Contact = () => {
 	const contactRef = useNav('contact');
@@ -31,13 +11,7 @@ const Contact = () => {
 	return (
 		<section ref={contactRef} className='contactContainer'>
 			<h3>CONTACT</h3>
-			<div className='icons'>
-				{links.map(({ id, src, href, alt }) => (
-					<a key={id} href={href} target='_blank' rel='noreferrer'>
-						<img className='linkIcon' src={src} alt={alt} />
-					</a>
-				))}
-			</div>
+			<ContactBtns />
 			<ContactBar />
 		</section>
 	);
---
+++ src/main/ContactBtns.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './ContactBtns.css';
+
+const links = [
+	{
+		id: 1,
+		src: '/octocat.svg',
+		href: 'https://github.com/shapirodaniel',
+		alt: 'github',
+	},
+	{
+		id: 2,
+		src: '/linked-in.svg',
+		href: 'https://linkedin.com/in/shapirodanieladam',
+		alt: 'linkedin',
+	},
+	{
+		id: 3,
+		src: '/medium.svg',
+		href: 'https://shapirodanieladam.medium.com',
+		alt: 'medium',
+	},
+];
+
+const ContactBtns = () => (
+	<div className='icons'>
+		{links.map(({ id, src, href, alt }) => (
+			<a key={id} href={href} target='_blank' rel='noreferrer'>
+				<img className='linkIcon' src={src} alt={alt} />
+			</a>
+		))}
+	</div>
+);
+
+export default ContactBtns;
