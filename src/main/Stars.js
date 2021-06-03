1a77c87385dc9f3ab1a4eaf2be87b539ea7c878f
shapirodaniel <shapirodanieladam@gmail.com>
2021-06-03 12:46:37 -0500

    add: media queries for contact layout

--- src/main/Stars.js
+++ src/main/Stars.js
@@ -1,11 +1,5 @@
-import React, { useRef, useEffect } from 'react';
-import { useOnScreen } from '../customHooks/useOnScreen';
+import React from 'react';
 import './Stars.css';
-import './rocket-ship/rocketShip.css';
-import './rocket-ship/speed.css';
-import './rocket-ship/leftJet.css';
-import './rocket-ship/rightJet.css';
-import './rocket-ship/rocketShipOrbit.css';
 
 let starsArray = new Array(400).fill(null).map((_star, idx) => (
 	<span
@@ -30,77 +24,11 @@ let starsArray = new Array(400).fill(null).map((_star, idx) => (
 	 when the first rocket container appears in view */
 
 const Stars = ({ children }) => {
-	const rocketContainerAcross = useRef(null);
-	const rocketShip = useRef(null);
-	const speed = useRef(null);
-	const leftJet = useRef(null);
-	const rightJet = useRef(null);
-
-	const rocketContainerOrbit = useRef(null);
-	const rocketShipOrbit = useRef(null);
-	const rocketShipTrail = useRef(null);
-
-	const isOnScreen = useOnScreen(rocketContainerAcross);
-
-	useEffect(() => {
-		const refs = [
-			{ ref: rocketContainerAcross, name: 'rocketContainerAcross' },
-			{ ref: rocketShip, name: 'rocketShip' },
-			{ ref: speed, name: 'speed' },
-			{ ref: leftJet, name: 'leftJet' },
-			{ ref: rightJet, name: 'rightJet' },
-			{
-				ref: rocketContainerOrbit,
-				name: 'rocketContainerOrbit',
-			},
-			{ ref: rocketShipOrbit, name: 'rocketShipOrbit' },
-			{ ref: rocketShipTrail, name: 'rocketShipTrail' },
-		];
-
-		if (isOnScreen) {
-			console.log('rocketcontainer onscreen!');
-			refs.forEach(({ ref, name }) => (ref.current.className = name));
-		}
-	}, [isOnScreen]);
-
 	return (
 		<section className='skyContainer'>
 			{children}
 			{starsArray}
 
-			{/* initial className is "ignore" to hide svgs */}
-
-			{/* forward animation on intersection with viewport */}
-			<div className='rocketContainerAcross ignore' ref={rocketContainerAcross}>
-				<img
-					className='ignore'
-					ref={rocketShip}
-					src={'/rocket-ship.svg'}
-					alt='rocket-ship'
-				/>
-				<img className='ignore' ref={speed} src={'/speed.svg'} alt='speed' />
-				<img className='ignore' ref={leftJet} src={'/jet.svg'} alt='jet' />
-				<img className='ignore' ref={rightJet} src={'/jet.svg'} alt='jet' />
-			</div>
-
-			{/* infinite animation "orbiting" .pinkMoon img,
-			scaled down on approach to give depth illusion */}
-			<div className='rocketContainerOrbit ignore' ref={rocketContainerOrbit}>
-				<div>
-					<img
-						className='ignore'
-						ref={rocketShipOrbit}
-						src={'/rocket-ship.svg'}
-						alt='rocket-ship'
-					/>
-					<img
-						className='ignore'
-						ref={rocketShipTrail}
-						src={'/rocket-ship-trail.svg'}
-						alt='rocket-ship-trail'
-					/>
-				</div>
-			</div>
 			<img className='pinkMoon' src='/pink-moon.png' alt='pink-moon' />
 		</section>
 	);
