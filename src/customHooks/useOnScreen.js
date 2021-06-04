33961f3bc42e39fbc8e4b3c235232a60dbf0e69d
shapirodaniel <shapirodanieladam@gmail.com>
2021-06-03 20:18:03 -0500

    fix: intersectionObserver custom hook threshold vals

--- src/customHooks/useOnScreen.js
+++ src/customHooks/useOnScreen.js
@@ -6,7 +6,7 @@ export const useOnScreen = ref => {
 	const observer = new IntersectionObserver(
 		([entry]) => setOnScreen(entry.isIntersecting),
 		{
-			threshold: [0.2, 0.4, 0.6, 0.8],
+			threshold: [0.25, 0.5, 0.75],
 		}
 	);
 
